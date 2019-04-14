8bea2d3153ea36fe8966d3b13b2986374661ae91
V Dheeraj <41289602+dheerajrox@users.noreply.github.com>
2019-04-14 22:46:33 +0530

    Update 5_1.s

--- 5_1.s
+++ 5_1.s
@@ -1,28 +1,77 @@
-
 .data
 
+
+num1: .word 5
+num2: .word 11
 
 .text
 .globl main
 .ent main
 main:
+	
 
-addiu $t0, $zero, 29932
-sll $t0, $t0, 16
-addiu $t0, $t0, 29932
 
-addiu $t1, $zero, 19232
-sll $t1, $t1, 16
-addiu $t1, $t1, 19232
+lw $t1, num1
+lw $t2, num2
+li $a0, 0 #for answer
+li $a1, 0 #for answer
 
-mult $t0, $t1
+li $a2, 1 
+sll $a2, $a2, 31 #mask
 
-mfhi $s0
+li $t0, 1 #counter
+li $a3, 1 #checking
+
+loop:
+
+and $t3, $t0, $t1
+
+sll $t0,$t0,1
+
+bgtz $t3, add_num2_to_a0
+
+
+
+j continue
+
+add_num2_to_a0:
+
+and $t5, $a0, $a2
+
+bgtz $t5, leftshifta1_add_1_to_a1
+
+j con1
+
+leftshifta1_add_1_to_a1:
+
+sll $a1, $a1, 1
+add $a1, $a1, $a3
+
+con1:
+
+mult $t3, $t2
 mflo $s1
 
-#the first 32 bits of the answer is stored in s1 register and the next 32 bits are stored in s0 register. 
+add $a0, $s1, $a0
+
+continue:
+
+
+
+
+
+beq $t0, $a2, exit
+
+j loop
+
+exit:
+
+
+
+
 
 li $v0, 10
 syscall
 
 .end main
+
